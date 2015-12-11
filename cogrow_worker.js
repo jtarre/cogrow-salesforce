4c454b13c5525bbdb13c8405569be4d837c77eec
Jason Tarre <jason.tarre@gmail.com>
2015-12-10 20:49:50 -0500

    got config variables working. ironworker documentation is poor.

--- cogrow_worker.js
+++ cogrow_worker.js
@@ -1,7 +1,25 @@
-var worker = require('node_helper');
+var worker  = require('node_helper');
+var jsforce = require('jsforce');
+
 var campaignId;
+
+var conn;
 // console.log("Hello world from Node.js");
 console.log("params:", worker.params);
+console.log("login params", worker.config.LOGIN_URL);
+
+conn = new jsforce.Connection({
+    loginUrl : worker.config.LOGIN_URL,
+    clientSecret: worker.config.CLIENT_SECRET, 
+    redirectUri: worker.config.REDIRECT_URI,
+    clientId: worker.config.CLIENT_ID,
+    instanceUrl: worker.config.INSTANCE_URL
+});
+
+conn.log(worker.config.USER_EMAIl, worker.config.PASSWORD, function(err, userInfo) {
+	if(err) {return console.error(err);}
+	console.log(userInfo);
+})
 
 campaignId = worker.params.campaignId;
 console.log("Please be so kind and tell me the campaign id:", campaignId);
