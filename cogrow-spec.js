fbb0a2c6666a0896bc1f3b5d3062fe2100b78535
Jason Tarre <jason.tarre@gmail.com>
2015-12-10 20:00:59 -0500

    ironworker working so far

---
+++ cogrow-spec.js
@@ -0,0 +1,5 @@
+
+describe("CoGrow Test") {
+	it("pull in data")
+	// etc. etc. 
+}
--- cogrow_worker.js
+++ cogrow_worker.js
@@ -1,6 +1,9 @@
 var worker = require('node_helper');
-
-console.log("Hello world from Node.js");
+var campaignId;
+// console.log("Hello world from Node.js");
 console.log("params:", worker.params);
+
+campaignId = worker.params.campaignId;
+console.log("Please be so kind and tell me the campaign id:", campaignId);
 console.log("config:", worker.config);
 console.log("task_id:", worker.task_id);
